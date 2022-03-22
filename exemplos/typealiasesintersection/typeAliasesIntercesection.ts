32b2351361330fed2a31ced30a1e15e754171d6c
Caetano <lcaetano30@gmail.com>
2022-03-22 11:44:12 -0300

    Aliases and Intesection

---
+++ exemplos/typealiasesintersection/typeAliasesIntercesection.ts
@@ -0,0 +1,35 @@
+
+//accuntInfo
+//charInfo
+// playerInfo
+
+type AccountInfo = {
+    id : number;
+    name: string;
+    email?: string; //* ? -> Torna o parâmetro opcional , para ser chamado na impelentação do objeto
+}
+const account: AccountInfo ={
+    id : 123,
+    name : "Leandro",
+    email : "l@gmail.com" //*
+}
+
+type CharInfo = {
+    nickname : string;
+    level : number;
+}
+const char: CharInfo = {
+    nickname: "Caetano",
+    level = 100
+}
+
+// intersection
+type PlayerInfo = AccountInfo & CharInfo // -> União de dois objetos
+
+const  player: PlayerInfo = {
+    id : 123,
+    name : "Leandro",
+    email : "l@gmail.com",
+    nickname: "Caetano",
+    level = 100
+}
